a83f333d87291dcf1149deb0b393bfc6446ec2ed
Aryan <aryandiffract@gmail.com>
2020-10-07 09:28:59 +0530

    Added files via upload

---
+++ reorder_stuff.rb
@@ -0,0 +1,12 @@
+def alphabetize(arr, rev=false)
+  if rev
+    arr.sort { |item1, item2| item2 <=> item1 }
+  else
+    arr.sort { |item1, item2| item1 <=> item2 }
+  end
+end
+
+books = ["Heart of Darkness", "Code Complete", "The Lorax", "The Prophet", "Absalom, Absalom!"]
+
+puts "A-Z ordering: #{alphabetize(books)}"
+puts "Z-A ordering: #{alphabetize(books, true)}"
