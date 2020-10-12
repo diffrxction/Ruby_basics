85f68b92a6c8b2ed5b6be50ed3b61179a3df6aaa
Aryan <aryandiffract@gmail.com>
2020-10-12 21:21:04 +0530

    Create rubycoolsyntax.rb

---
+++ rubycoolsyntax.rb
@@ -0,0 +1,8 @@
+=begin
+Note: 
+This file is a compilation of cool syntaxes that Ruby has to offer. I will add more whenever I'm amazed at how programmer-friendly Ruby can get.
+Also using these functions/objects/keywords may be rare, but I have realized that they can simplify the process of algorithm implementation considerably.
+=end
+"L".upto("P") do |char|
+  puts char
+end
