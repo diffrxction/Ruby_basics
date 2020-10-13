af5e99c0677c1bfc46f1a5a2a07bc5b672f4137b
Aryan <aryandiffract@gmail.com>
2020-10-13 06:05:35 +0530

    Update rubycoolsyntax.rb

--- rubycoolsyntax.rb
+++ rubycoolsyntax.rb
@@ -6,3 +6,9 @@ Also using these functions/objects/keywords may be rare, but I have realized tha
 "L".upto("P") do |char|
   puts char
 end
+=begin
+.respond_to? takes a symbol and returns true if an object can receive that method and false otherwise.
+.next will return the integer immediately following the integer it’s called on, meaning 4.next will return 5
+=end
+age = 22
+puts age.respond_to?(:next)
